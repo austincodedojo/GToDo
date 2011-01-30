b768480e0b746c5cb8af9b0753aeb676f4c2d3f6
Duke Banerjee <dbanerje@sbcglobal.net>
2011-01-30 00:50:15 -0600

    Get ready for populating task lists

--- test/functional/src/my/apps/Lists.java
+++
@@ -1,8 +0,0 @@
-package my.apps;
-
-import android.provider.BaseColumns;
-
-public class Lists implements BaseColumns {
-    public static String ID = "id";
-    public static final String NAME = "name";
-}
